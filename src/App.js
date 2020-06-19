652e27da6c3d99542cdc50ca9cba80e43ff27233
arslan456 <muhammadarslanjabbar@gmail.com>
2020-06-19 07:45:28 +0500

    third repo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState} from 'react';
+import {Message} from './Message';
 import './App.css';
 
-function App() {
+export default function App() {
+  let [count,setCount]=useState(1)
+  let [isMorning,setMorning]=useState(true)
   return (
-    <div>
-      Hello World
+    <div className={`box ${isMorning ? 'dayLight' : ''}`}>
+      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
+
+     <Message counter={count}/>
+     <br/>
+     <button onClick={
+       ()=> setCount(++count) }>
+         Update Counter
+         </button>
+         <button onClick={()=>setMorning(!isMorning)}>Update day
+         </button>
     </div>
   );
 }
 
-export default App;
+
