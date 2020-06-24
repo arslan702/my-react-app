6c09d65aa733e0eeadfde0d29b0461390fef3dee
arslan456 <muhammadarslanjabbar@gmail.com>
2020-06-24 19:03:03 +0500

    practice session

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React,{useState} from 'react';
-import {Message} from './Message';
+import {Message} from './Message.js'
 import './App.css';
 
 export default function App() {
-  let [count,setCount]=useState(1)
-  let [isMorning,setMorning]=useState(true)
-  return (
+  let[count,setCount]=useState(1)
+  let[isMorning,setMorning]=useState(true);
+  return(
     <div className={`box ${isMorning ? 'dayLight' : ''}`}>
-      <h1>Good {isMorning ? 'Morning' : 'Night'}</h1>
-
-     <Message counter={count}/>
-     <br/>
-     <button onClick={
-       ()=> setCount(++count) }>
-         Update Counter
-         </button>
-         <button onClick={()=>setMorning(!isMorning)}>Update day
-         </button>
+      <h1>Day time={isMorning ? 'Morning' : 'Night'}</h1>
+      <Message counter={count}/>
+      <br/>
+      <button onClick={
+        ()=>setCount(count+1)}>
+        update counter</button>
+        <button onClick={()=>setMorning(!isMorning)}>update day</button>
     </div>
-  );
+  )
 }
 
 
--- src/Message.js
+++ src/Message.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export function Message(props){
     return(
-        <h3>value of counter var is : {props.counter}</h3>
+    <h3>Value of counter varialbe is: {props.counter}</h3>
     )
 }
